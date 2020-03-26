a517507aafca584cb66fd411dfe6f912ea192f4e
Albertino Padin <albertino.padin@upr.edu>
2020-03-26 02:59:41 -0700

    Initial commit for toy project to learn SpriteKit in 2020.

--- SKPong/GameScene.swift
+++ SKPong/GameScene.swift
@@ -13,60 +13,58 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var palletNode: SKShapeNode?
+    
+    private var palletNodeY: CGFloat?
+    private var palletNodeXMinBound: CGFloat?
+    private var palletNodeXMaxBound: CGFloat?
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        let palletNodeSize = CGSize.init(width: self.size.width/5, height: self.size.height/40)
+        self.palletNode = SKShapeNode.init(rectOf: palletNodeSize, cornerRadius: 10.0)
+        self.palletNode?.fillColor = .blue
+        palletNodeY = palletNodeSize.height + 20
+        palletNodeXMinBound = palletNodeSize.width  // How does this even work, should be /2...
+        palletNodeXMaxBound = self.frame.width - palletNodeXMinBound!
+        let initialPosition = CGPoint(x: self.frame.midX, y: palletNodeY!)
+        self.palletNode?.position = initialPosition
+        self.addChild(self.palletNode!)
+    }
+    
+    
+    func calculatePalletNodeXPosition(touchPosition: CGPoint) -> CGFloat {
+        if touchPosition.x < palletNodeXMinBound! {
+            return palletNodeXMinBound!
+        } else if touchPosition.x > palletNodeXMaxBound! {
+            return palletNodeXMaxBound!
+        } else {
+            return touchPosition.x
         }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+//        if let n = self.palletNode {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
+        if let n = self.palletNode {
+            n.position.x = calculatePalletNodeXPosition(touchPosition: pos)
             n.strokeColor = SKColor.blue
-            self.addChild(n)
         }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
+        if let n = self.palletNode {
+            n.position.x = calculatePalletNodeXPosition(touchPosition: pos)
             n.strokeColor = SKColor.red
-            self.addChild(n)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
