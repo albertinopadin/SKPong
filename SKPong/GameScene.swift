b2fedbef0cb0874031ba7169032ac1b602d39cc5
Albertino Padin <albertino.padin@upr.edu>
2020-03-31 03:13:53 -0700

    Added walls.

--- SKPong/GameScene.swift
+++ SKPong/GameScene.swift
@@ -11,10 +11,17 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
+//    private var label : SKLabelNode?
     private var topPalletNode: SKShapeNode?
     private var bottomPalletNode: SKShapeNode?
     private var ballNode: SKShapeNode?
+    
+    private var leftWallNode: SKShapeNode?
+    private var rightWallNode: SKShapeNode?
+    
+    // For testing only
+    private var topWallNode: SKShapeNode?
+    private var bottomWallNode: SKShapeNode?
     
     private let palletSeparation: CGFloat = 20.0
     private var topPalletNodeY: CGFloat?
@@ -22,10 +29,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var palletNodeXMinBound: CGFloat?
     private var palletNodeXMaxBound: CGFloat?
     
-    private let palletCategory: UInt32 = 0x1 << 1
-    private let ballCategory: UInt32 = 0x1 << 0
+    private let ballCategory: UInt32    = 0x1 << 0
+    private let palletCategory: UInt32  = 0x1 << 1
+    private let wallCategory: UInt32    = 0x1 << 2
+    
+    private let initialBallVelocity = -300.0
     
     override func didMove(to view: SKView) {
+        // Debugging:
+        print("Frame: \(self.frame)")
+        
         // Instantiate Pallet Nodes:
         let palletNodeSize = CGSize(width: self.size.width/5, height: self.size.height/40)
         self.palletNodeXMinBound = palletNodeSize.width  // How does this even work, should be /2...
@@ -47,15 +60,38 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.ballNode = self.createBallNode(radius: ballRadius, position: initialBallPosition)
         self.addChild(self.ballNode!)
         
+        // Instantiate Wall Nodes:
+        let sideWallSize = CGSize(width: 10.0, height: self.size.height)
+        let leftSideWallPosition = CGPoint(x: self.frame.minX + 40, y: self.frame.midY)
+        self.leftWallNode = self.createWallNode(size: sideWallSize, position: leftSideWallPosition)
+        self.addChild(self.leftWallNode!)
+        
+        let rightSideWallPosition = CGPoint(x: self.frame.maxX - 40, y: self.frame.midY)
+        self.rightWallNode = self.createWallNode(size: sideWallSize, position: rightSideWallPosition)
+        self.addChild(self.rightWallNode!)
+        
+        let topBottomWallSize = CGSize(width: self.size.width, height: 10.0)
+        let topWallPosition = CGPoint(x: self.frame.midX, y: self.frame.maxY)
+        self.topWallNode = self.createWallNode(size: topBottomWallSize, position: topWallPosition)
+        self.addChild(self.topWallNode!)
+        
+        let bottomWallPosition = CGPoint(x: self.frame.midX, y: self.frame.minY)
+        self.bottomWallNode = self.createWallNode(size: topBottomWallSize, position: bottomWallPosition)
+        self.addChild(self.bottomWallNode!)
+        
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         self.physicsWorld.contactDelegate = self
+        
+        // TODO: Figure out why side walls are not where they should be
+        // TODO: If ball is going perfectly horizontal, randomly impart up/down impulse
+        // TODO: If ball is going perfectly vertical near a wall, randomly impart left/right impulse.
     }
     
     func createPalletNode(size: CGSize, position: CGPoint) -> SKShapeNode {
         let pallet = SKShapeNode(rectOf: size, cornerRadius: 10.0)
+        pallet.position = position
         pallet.fillColor = .blue
         pallet.strokeColor = .white
-        pallet.position = position
         pallet.physicsBody = SKPhysicsBody(rectangleOf: size)
         pallet.physicsBody?.isDynamic = false
         pallet.physicsBody?.restitution = 1.0
@@ -66,10 +102,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func createBallNode(radius: CGFloat, position: CGPoint) -> SKShapeNode {
-        let ball = SKShapeNode.init(circleOfRadius: radius)
+        let ball = SKShapeNode(circleOfRadius: radius)
+        ball.position = position
         ball.fillColor = .yellow
         ball.strokeColor = .white
-        ball.position = position
         ball.physicsBody = SKPhysicsBody(circleOfRadius: radius)
         ball.physicsBody?.isDynamic = true
         ball.physicsBody?.restitution = 1.0
@@ -77,6 +113,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.physicsBody?.categoryBitMask = ballCategory
         ball.physicsBody?.contactTestBitMask = palletCategory
         return ball
+    }
+    
+    func createWallNode(size: CGSize, position: CGPoint) -> SKShapeNode {
+        let wall = SKShapeNode(rectOf: size)
+        wall.position = position
+        wall.fillColor = .cyan
+        wall.strokeColor = .lightGray
+        wall.physicsBody = SKPhysicsBody(rectangleOf: size)
+        wall.physicsBody?.isDynamic = false
+        wall.physicsBody?.restitution = 1.01
+        wall.physicsBody?.friction = 0.0
+        wall.physicsBody?.categoryBitMask = wallCategory
+        wall.physicsBody?.contactTestBitMask = ballCategory
+        return wall
     }
     
     func calculatePalletNodeXPosition(touchPosition: CGPoint) -> CGFloat {
@@ -96,7 +146,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func touchDown(atPoint pos : CGPoint) {
         if let ball = self.ballNode {
             if ball.physicsBody?.velocity.dx == 0.0 && ball.physicsBody?.velocity.dy == 0.0 {
-                ball.physicsBody?.velocity = CGVector(dx: 0.0, dy: -200.0)
+                ball.physicsBody?.velocity = CGVector(dx: 0.0, dy: self.initialBallVelocity)
             }
         }
     }
